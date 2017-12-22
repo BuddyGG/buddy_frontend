79a042bc812e60326eeb7bcd7efc0f0dce734bae
esow <simmerkaer@gmail.com>
2017-12-22 22:14:22 +0100

    fixed champion names with special chars

--- src/config/ChampImages.jsx
+++ src/config/ChampImages.jsx
@@ -4,14 +4,19 @@ import { Image } from 'semantic-ui-react'
 
 export function getChampImage(champ){
     let trimmedChamp = champ.replace(/\s/g,''); // Remove whitespace in champ name
-    if(trimmedChamp === "Cho'Gath"){ //TODO: Fix
-        trimmedChamp = "Chogath"
-    }
-    const champImageUrl = "https://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + trimmedChamp + ".png";
+    let trimmedChamp2 = trimmedChamp.replace("'",""); // Replace ' with whitespace
+    let allLoverCase = trimmedChamp2.toLowerCase();
+    let championString = capitalizeFirstLetter(allLoverCase);
+
+    const champImageUrl = "https://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + championString + ".png";
     return <Image centered src={champImageUrl} size='mini'/>;
 }
 
 export function getSummonerIcon(id){
     const champImageUrl = "https://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/" + id + ".png";
     return <Image shape="rounded" src={champImageUrl} size='tiny'/>;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
