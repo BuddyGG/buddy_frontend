5b81a8b3af12bc8f1caf69bfab0a3f66dae9aa79
shau <shau@itu.dk>
2018-01-07 15:46:00 +0100

    fixed busy response bug

--- src/scenes/Matching.jsx
+++ src/scenes/Matching.jsx
@@ -67,7 +67,8 @@ export default class Matching extends Component {
 
 
 
-            const playerIsBusy = this.state.requestedModalOpen
+            const playerIsBusy = this.state.requestedModalOpen || this.state.requestingModalOpen
+
 
             if (playerIsBusy){
                 const responseMessage = {
