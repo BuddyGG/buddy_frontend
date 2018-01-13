bde7dc8f567856aa2dbe29285e497693c4ac6bb7
esow <simmerkaer@gmail.com>
2018-01-13 11:37:05 +0100

    Fixed outlying champ names

--- src/config/ChampImages.jsx
+++ src/config/ChampImages.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 
+const outliers = ["Wukong","Jarvan IV","Kog'Maw","Dr. Mundo","Vel'Koz"];
 
 export function getChampImage(champ){
-    let trimmedChamp = champ.replace(/\s/g,''); // Remove whitespace in champ name
-    let trimmedChamp2 = trimmedChamp.replace("'",""); // Replace ' with whitespace
-    let allLoverCase = trimmedChamp2.toLowerCase();
-    let championString = capitalizeFirstLetter(allLoverCase);
+    let championString;
 
+    if(outliers.includes(champ)){
+        championString = handleOutliers(champ)
+    } else {
+        let trimmedChamp = champ.replace(/\s/g,''); // Remove whitespace in champ name
+        let trimmedChamp2 = trimmedChamp.replace("'",""); // Replace ' with whitespace
+        let allLoverCase = trimmedChamp2.toLowerCase();
+        championString = capitalizeFirstLetter(allLoverCase);
+    }
+    
     const champImageUrl = "https://ddragon.leagueoflegends.com/cdn/7.24.1/img/champion/" + championString + ".png";
     return <Image  centered shape="circular" src={champImageUrl} size='mini'/>;
 }
@@ -19,4 +26,35 @@ export function getSummonerIcon(id){
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function handleOutliers (champ){
+    let convertedChamp;
+
+    switch(champ) {
+        case "Wukong":
+            convertedChamp = "MonkeyKing";
+            break;
+            
+        case "Jarvan IV":
+            convertedChamp = "JarvanIV";
+            break;
+
+        case "Kog'Maw":
+            convertedChamp = "KogMaw";   
+            break;
+
+        case "Dr. Mundo":
+            convertedChamp = "DrMundo";   
+            break;
+
+        case "Vel'Koz":
+            convertedChamp = "Velkoz";   
+            break;
+               
+        default:
+            break;
+    }
+
+    return convertedChamp;
 }
