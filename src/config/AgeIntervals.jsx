dbfa3d3ab140c9dc750d499dd33a88f5130940ab
esow <simmerkaer@gmail.com>
2018-03-03 18:13:11 +0100

    fixed bug with age interval

--- src/config/AgeIntervals.jsx
+++ src/config/AgeIntervals.jsx
@@ -3,7 +3,7 @@ export const interval2 = "20-29"
 export const interval3 = "29+"
 
 export const ageGroups = {
-    interval1: "13-1",
+    interval1: "13-19",
     interval2: "20-29",
     interval3: "29+"
 }
