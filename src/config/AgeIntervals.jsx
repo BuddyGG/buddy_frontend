98d5c8ef95dacb569616c58fa3f867181b809507
esow <simmerkaer@gmail.com>
2018-03-10 15:20:18 +0100

    Changed age groups, Closes #15

--- src/config/AgeIntervals.jsx
+++ src/config/AgeIntervals.jsx
@@ -1,9 +1,9 @@
-export const interval1 = "13-19"
-export const interval2 = "20-29"
-export const interval3 = "29+"
+export const interval1 = "13-16"
+export const interval2 = "17-21"
+export const interval3 = "22+"
 
 export const ageGroups = {
-    interval1: "13-19",
-    interval2: "20-29",
-    interval3: "29+"
+    interval1: "13-16",
+    interval2: "17-21",
+    interval3: "22+"
 }
