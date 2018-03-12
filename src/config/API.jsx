c7baca492836f0d7b7e7ab2e0edfe75272a1cdcd
esow <simmerkaer@gmail.com>
2018-03-12 20:39:29 +0100

    running test env

--- src/config/API.jsx
+++ src/config/API.jsx
@@ -1,7 +1,7 @@
 // Test
-// export const backend_service = "https://lolbuddy.herokuapp.com/api"
-// export const socket_service = "wss://lolbuddy.herokuapp.com/socket"
+export const backend_service = "https://lolbuddy.herokuapp.com/api"
+export const socket_service = "wss://lolbuddy.herokuapp.com/socket"
 
 // Production
-export const backend_service = "https://api.buddy.gg/api";
-export const socket_service = "wss://api.buddy.gg/socket"
+// export const backend_service = "https://api.buddy.gg/api";
+// export const socket_service = "wss://api.buddy.gg/socket"
